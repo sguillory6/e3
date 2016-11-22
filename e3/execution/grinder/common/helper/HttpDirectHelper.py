ceb2d89bcd72468080581666b34fa82cf0c30762
Felix Haehnel <fhaehnel@atlassian.com>
2016-11-23 09:16:00 +1100

    Refactor SCM Helpers to use user object

--- e3/execution/grinder/common/helper/HttpDirectHelper.py
+++ e3/execution/grinder/common/helper/HttpDirectHelper.py
@@ -1,24 +1,19 @@
 from TestDataProvider import TestDataProvider
+from common.helper.ProtocolHelper import ProtocolHelper
 
 
-class HttpDirectHelper:
+class HttpDirectHelper(ProtocolHelper):
     def __init__(self):
-        self.test_data = TestDataProvider()
+        test_data = TestDataProvider()
+        super(HttpDirectHelper, self).__init__(test_data, test_data.choose_http_user_at_random())
 
-    def choose_user_at_random(self):
-        return self.test_data.choose_http_user_at_random()
-
-    def make_url(self, repo, user):
-        url = self.test_data.base_url
+    def make_url(self, repo):
+        url = self.base_url
         if ':7990' not in url:
             url += '7990'
 
         url_parts = url.split("://")
-        url_parts[1] = user['username'] + ":" + user['password'] + "@" + url_parts[1]
+        url_parts[1] = self.user['username'] + ":" + self.user['password'] + "@" + url_parts[1]
         url = "://".join(url_parts)
 
         return "%s/scm/%s.git" % (url, repo)
-
-    @staticmethod
-    def environment(ignore):
-        return {}
--- e3/execution/grinder/scm/base/GitClone.py
+++ e3/execution/grinder/scm/base/GitClone.py
@@ -9,15 +9,14 @@ class GitClone(TestScript):
     def __init__(self, number, args, helper):
         super(GitClone, self).__init__(number, args)
         self.protocol_helper = helper
-        self.username = self.protocol_helper.choose_user_at_random()
 
     def __call__(self):
         repo = self.test_data.random_project_repo_slug()
-        url = self.protocol_helper.make_url(repo, self.username)
+        url = self.protocol_helper.make_url(repo)
 
         temp_dir = tempfile.mkdtemp(dir=self.e3_temp_dir)
         try:
-            self.run_git(["clone", url, temp_dir], self.runner, env=self.protocol_helper.environment(self.username))
+            self.run_git(["clone", url, temp_dir], self.runner, env=self.protocol_helper.environment())
         finally:
             try:
                 shutil.rmtree(temp_dir)
