a59be5d4504c5644ed8252c706f7b46c4ec11cb3
Felix Haehnel <fhaehnel@atlassian.com>
2016-11-23 15:58:56 +1100

    Add User temp directory

--- e3/execution/grinder/common/helper/ProtocolHelper.py
+++ e3/execution/grinder/common/helper/ProtocolHelper.py
@@ -1,10 +1,12 @@
+import os
+from java.lang import System
 from common.wrapper.User import User
 
 
 class ProtocolHelper(object):
     def __init__(self, test_data, user_dict):
         self.base_url = test_data.base_url
-        self.user = User.from_dict(user_dict)
+        self.user = User.from_dict(user_dict, os.path.join(System.getProperty("root"), "tmp"))
         self.user.create_home()
 
     def environment(self):
