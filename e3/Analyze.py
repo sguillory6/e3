0315773de643eabe6a7506e22ba3dc5c5353444d
Charles Olivier <colivier@atlassian.com>
2016-11-24 17:15:15 +1100

    Added -n flag to Analyze.py to suppress opening of external viewer for headless operation.

--- e3/Analyze.py
+++ e3/Analyze.py
@@ -6,6 +6,7 @@ from analysis.rrdtool.Analyze import graph_experiment
 from analysis.rrdtool.Report import experiment_report
 
 from common.E3 import e3
+from common.Utils import disable_external_tool
 
 
 def run_gnuplot_analysis(run_name):
@@ -22,8 +23,13 @@ def run_rrd_analysis(run_name):
 @click.command()
 @click.option('-r', '--run', required=True, help='The experiment run you want to execute',
               type=click.Choice(e3.get_runs()), default=e3.get_single_run())
-def command(run):
+@click.option('-n', '--nolaunch', flag_value=True, default=False, help="Don't load artifacts in external viewer")
+def command(run, nolaunch):
     logging.config.fileConfig(e3.get_logging_conf())
+
+    if nolaunch:
+        disable_external_tool()
+
     run_rrd_analysis(run)
     run_gnuplot_analysis(run)
 
